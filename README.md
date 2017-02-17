3f3507d689260d24af40d03232c886a51b719e3d
Robert Martin <rmartin@rmart.in>
2017-02-17 17:36:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# li-permit-tracker
+This application allows users to track their L+I permit application by its ID.
+It uses the [pattern portfolio](http://cityofphiladelphia.github.io/patterns/)
+to provide a consistent experience. It was forked from the [311 service request tracker](https://github.com/CityOfPhiladelphia/service-request-tracker).
---
+++ main.js
@@ -0,0 +1,56 @@
+(function ($, _) {
+  var endpoint = '//gis.phila.gov/arcgis/rest/services/LNI/LI_PERMIT_APPLICATION_STATUS/FeatureServer/1/query'
+  var params = qs(window.location.search.substr(1))
+  // Use mustache.js style brackets in templates
+  _.templateSettings = { interpolate: /\{\{(.+?)\}\}/g }
+  var templates = {
+    result: _.template($('#tmpl-result').html()),
+    error: _.template($('#tmpl-error').html()),
+    loading: $('#tmpl-loading').html()
+  }
+  var resultContainer = $('#result')
+
+  if (params.id) {
+    resultContainer.html(templates.loading)
+    var requestParams = {
+      where: 'APNO = ' + params.id,
+      outFields: '*',
+      f: 'pjson',
+    }
+    $.getJSON(endpoint, requestParams, function (response) {
+      var features = response.features
+      if (features.length < 1) {
+        // If there's no feature, indicate such
+        resultContainer.html(templates.error({ service_request_id: params.id }))
+      } else {
+        // Otherwise display the first feature (which should be the only
+        // feature)
+
+        // Rename/manipulate API response to fit our HTML template
+        var attrs = response.features[0].attributes
+        var templateData = {
+          application_number:     attrs.APNO,
+          comments:               attrs.COMMENTS,
+        }
+
+        // Render template
+        resultContainer.html(templates.result(templateData))
+      }
+    }).fail(function () {
+      resultContainer.html(templates.error({ permit_id : params.id }))
+    })
+  }
+
+  // decode a uri into a kv representation :: str -> obj
+  // https://github.com/yoshuawuyts/sheet-router/blob/master/qs.js
+  function qs (uri) {
+    var obj = {}
+    var reg = new RegExp('([^?=&]+)(=([^&]*))?', 'g')
+    uri.replace(/^.*\?/, '').replace(reg, map)
+    return obj
+
+    function map (a0, a1, a2, a3) {
+      obj[window.decodeURIComponent(a1)] = window.decodeURIComponent(a3)
+    }
+  }
+})(window.jQuery, window._)
