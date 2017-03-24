04e907cab6eb564d2e2fd6448584d0a34055d753
acnewell <acnewell3@gmail.com>
2017-03-24 11:36:45 -0400

    attempt #6
    else if change

--- main.js
+++ main.js
@@ -52,7 +52,7 @@
         }
 		// added in else if, only change
 		
-		else (status === 'INCOMPLETE' && !suspdt ) {
+		else if (status === 'INCOMPLETE' && !suspdt ) {
 			comments = attrs.COMMENTS;
 		}
 
